c2abfff14fda8bc9b1f15efc4f6fa20e256d0ef9
ThiagoHenr01 <thiagohenrsilva@gmail.com>
2020-03-04 18:35:34 -0300

    Cronometro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            numero: 0,
+            botao: 'COMEÇAR'
+
+        };
+
+        this.timer = null;
+        this.comecar = this.comecar.bind(this);
+        this.zerar = this.zerar.bind(this);
+
+    };
+
+    comecar() {
+
+        let state = this.state;
+
+        if(this.timer !== null) {
+
+            clearInterval(this.timer);
+
+            this.timer = null;
+
+            state.botao = 'COMEÇAR';
+
+        } else {
+
+            this.timer = setInterval(() => {
+
+                let state = this.state;
+    
+                state.numero += 0.1;
+    
+                this.setState(state);
+    
+            }, 100);
+
+            state.botao = 'PAUSE';
+
+        };
+
+        this.setState(state);
+
+    };
+
+    zerar() {
+
+        if(this.timer !== null) {
+
+            clearInterval(this.timer);
+
+            this.timer = null;  
+
+        };
+
+        let state = this.state;
+
+        state.numero = 0;
+
+        state.botao = 'COMEÇAR';
+
+        this.setState(state);
+
+    };
+
+    render() {
+
+        return(
+
+            <div className="container" >
+
+                <img src={require('./assets/cronometro.png')} alt="Imagem de um cronômetro." className="img" />
+
+                <a className="timer" >{this.state.numero.toFixed(1)}</a>
+
+                <div className="areaBtn" >
+
+                    <a className="botao" onClick={this.comecar} >{this.state.botao}</a>
+                    <a className="botao" onClick={this.zerar} >ZERAR </a>
+
+                </div>
+
+            </div>
+
+        );
+
+    };
+
+};
 
 export default App;
